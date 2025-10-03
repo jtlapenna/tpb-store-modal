edd1a9c4fafdaf5b566f9beae13956284ea7659d
Jeff <jeff@thepeakbeyond.com>
2025-10-03 10:59:53 -0400

    Chore(theme): stop enqueuing modal assets in child theme; plugin owns modal

--- wp-content/themes/hello-elementor-child-tpb/functions.php
+++ wp-content/themes/hello-elementor-child-tpb/functions.php
@@ -14,45 +14,8 @@ function tpb_qv_is_iframe() {
     return false;
 }
 
-// Enqueue TPB QuickView assets (CSS/JS)
-function tpb_qv_enqueue_assets() {
-    $theme_uri = get_stylesheet_directory_uri();
-    $theme_dir = get_stylesheet_directory();
-
-    $css_files = [
-        'assets/css/tpb-qv.css',
-        'assets/css/tpb-quickview.css',
-    ];
-    foreach ($css_files as $rel) {
-        $path = $theme_dir . '/' . $rel;
-        if (file_exists($path)) {
-            wp_enqueue_style(
-                'tpb-' . md5($rel),
-                $theme_uri . '/' . $rel,
-                [],
-                filemtime($path)
-            );
-        }
-    }
-
-    $js_files = [
-        'assets/js/tpb-modal.js',
-        'assets/js/tpb-qv-iframe.js',
-    ];
-    foreach ($js_files as $rel) {
-        $path = $theme_dir . '/' . $rel;
-        if (file_exists($path)) {
-            wp_enqueue_script(
-                'tpb-' . md5($rel),
-                $theme_uri . '/' . $rel,
-                ['jquery'],
-                filemtime($path),
-                true
-            );
-        }
-    }
-}
-add_action('wp_enqueue_scripts', 'tpb_qv_enqueue_assets', 20);
+// Note: Modal assets are enqueued by the custom plugin (tpb-quickview-modal).
+// The child theme intentionally does not enqueue modal CSS/JS to avoid conflicts.
 
 // Add body class for iframe mode
 function tpb_qv_body_class($classes) {
