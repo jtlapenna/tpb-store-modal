90648ca5d2a76f6faf419aa1bf823860941416a1
Jeff <jeff@thepeakbeyond.com>
2025-10-26 12:18:40 -0700

    Hide cart icon when drawer opens, keep drawer above overlay
    - Hide cart icons (visibility + opacity) when drawer is open
    - Keep drawer z-index high (99999999) to appear above modal overlay
    - Show cart icons again when drawer closes
    - Drawer should now be above overlay, icon should be hidden

--- wp-content/plugins/tpb-quickview-modal/assets/js/modal-state.js
+++ wp-content/plugins/tpb-quickview-modal/assets/js/modal-state.js
@@ -74,12 +74,33 @@
             var cartObserver = new MutationObserver(function(mutations) {
                 mutations.forEach(function(mutation) {
                     // Check if cart drawer/panel is opening with more generic selectors
-                    var hasOpenDrawer = document.querySelectorAll('[class*="cart"][class*="open"], [class*="drawer"][class*="open"], [class*="panel"][class*="open"], [aria-expanded="true"][class*="cart"], .elementor-menu-cart__toggle_button[aria-expanded="true"]');
-                    if (hasOpenDrawer.length > 0) {
-                        console.log('🛒 Cart drawer is open, resetting cart z-index');
-                        console.log('🛒 Found open cart elements:', hasOpenDrawer.length);
-                        self.resetCartZIndex();
+                    var toggleButton = document.querySelector('.elementor-menu-cart__toggle_button[aria-expanded="true"]');
+                    var hasOpenDrawer = document.querySelectorAll('[class*="cart"][class*="open"], [class*="drawer"][class*="open"], [class*="panel"][class*="open"], [aria-expanded="true"][class*="cart"]');
+                    
+                    if (toggleButton || hasOpenDrawer.length > 0) {
+                        console.log('🛒 Cart drawer is open');
+                        // Hide the toggle button icon when drawer is open
+                        var cartIcons = document.querySelectorAll('.eicon-cart, [class*="cart-icon"], [class*="woocommerce-cart-icon"]');
+                        cartIcons.forEach(function(icon) {
+                            if (!icon.closest('.elementor-menu-cart__main')) {
+                                icon.style.visibility = 'hidden';
+                                icon.style.opacity = '0';
+                            }
+                        });
+                        
+                        // Ensure drawer itself has high z-index
+                        var drawer = document.querySelector('.elementor-menu-cart__main');
+                        if (drawer) {
+                            drawer.style.zIndex = '99999999';
+                        }
                     } else {
+                        // If drawer is not open, show icons and raise z-index for modal
+                        var cartIcons = document.querySelectorAll('.eicon-cart, [class*="cart-icon"], [class*="woocommerce-cart-icon"]');
+                        cartIcons.forEach(function(icon) {
+                            icon.style.visibility = '';
+                            icon.style.opacity = '';
+                        });
+                        
                         // If drawer is not open and modal is open, raise z-index
                         if (self.isOpen) {
                             self.raiseCartZIndex();
