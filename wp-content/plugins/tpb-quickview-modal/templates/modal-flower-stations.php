c2a1ab469fee5a5326e4ca861e77d76e40e9942a
Jeff <jeff@thepeakbeyond.com>
2025-10-27 14:30:48 -0700

    Fix Flower Station description to not mention base price on first load

--- wp-content/plugins/tpb-quickview-modal/templates/modal-flower-stations.php
+++ wp-content/plugins/tpb-quickview-modal/templates/modal-flower-stations.php
@@ -6,7 +6,7 @@
 
 $modal_type = 'flower-station';
 $title = 'Configure Your Flower Stations';
-$description = 'Please choose a SKU-count to see the base-price for electronics hardware. Furniture-inclusive pricing will appear below according to your selections.';
+$description = 'Please choose a SKU-count to configure your Flower Station hardware. Select from the options below to get started.';
 
 include plugin_dir_path(__FILE__) . 'parts/modal-base.php';
 ?>
